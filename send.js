aff142e0c78b16fe04e744433defedd4fb5dd97b
koyashiro <develop@koyashi.ro>
2022-02-16 22:44:49 +0900

    fix: fix and format

--- send.js
+++ send.js
@@ -1,87 +1,88 @@
 (() => {
-    const defaultMethod = 'POST';
-    let toVRC = (receiveUrl, method = defaultMethod) => {
-        const url = urlReplace(receiveUrl)
-        fetch('http://localhost:11400/url', {
-            method: method,
-            mode: 'cors',
-            credentials: 'omit',
-            cache: 'no-cache',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({url: url})
-        }).then((res) => {
-            if (!res.ok && method !== 'PUT') {
-                toVRC(url, 'PUT');
-            }
-
-            if (!res.ok && method === 'PUT') {
-                window.alert('please start send_vrc_desktop.');
-            }
-        }).catch((e) => {
-            console.log(e, method);
-            // old version compatible.
-            if (method !== 'PUT') {
-                toVRC(url, 'PUT');
-            }
-            if (method === 'PUT') {
-                window.alert('please start send_vrc_desktop.');
-            }
-        });
-    }
-    let getClipboard = () =>  {
-        const pasteTarget = document.createElement('div');
-        pasteTarget.contentEditable = 'true';
-        const actElem = document.activeElement.appendChild(pasteTarget).parentNode;
-        pasteTarget.focus();
-        document.execCommand('Paste', null, null);
-        const paste = pasteTarget.innerText;
-        actElem.removeChild(pasteTarget);
-        return paste;
-    }
-
-
-    let urlReplace = (url) =>  {
-        const u = new URL(url);
-        switch (u.hostname) {
-            case 'youtube.com':
-            case 'www.youtube.com':
-            case 'm.youtube.com':
-                const v = u.searchParams.get("v");
-                const allowURL = new URL("https://www.youtube.com/watch");
-                allowURL.searchParams.append("v", v)
-                return allowURL.toString();
+  const defaultMethod = "POST";
+  let toVRC = (receiveUrl, method = defaultMethod) => {
+    const url = urlReplace(receiveUrl);
+    fetch("http://localhost:11400/url", {
+      method: method,
+      mode: "cors",
+      credentials: "omit",
+      cache: "no-cache",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ url: url }),
+    })
+      .then((res) => {
+        if (!res.ok && method !== "PUT") {
+          toVRC(url, "PUT");
         }
 
+        if (!res.ok && method === "PUT") {
+          window.alert("please start send_vrc_desktop.");
+        }
+      })
+      .catch((e) => {
+        console.log(e, method);
+        // old version compatible.
+        if (method !== "PUT") {
+          toVRC(url, "PUT");
+        }
+        if (method === "PUT") {
+          window.alert("please start send_vrc_desktop.");
+        }
+      });
+  };
+  let getClipboard = () => {
+    const pasteTarget = document.createElement("div");
+    pasteTarget.contentEditable = "true";
+    const actElem = document.activeElement.appendChild(pasteTarget).parentNode;
+    pasteTarget.focus();
+    document.execCommand("Paste", null, null);
+    const paste = pasteTarget.innerText;
+    actElem.removeChild(pasteTarget);
+    return paste;
+  };
+
+  let urlReplace = (url) => {
+    const u = new URL(url);
+    switch (u.hostname) {
+      case "youtube.com":
+      case "www.youtube.com":
+      case "m.youtube.com": {
+        const v = u.searchParams.get("v");
+        const allowURL = new URL("https://www.youtube.com/watch");
+        allowURL.searchParams.append("v", v);
+        return allowURL.toString();
+      }
+      default:
         return u.toString();
     }
+  };
 
+  chrome.contextMenus.create({
+    title: "SendVRC this page",
+    type: "normal",
+    contexts: ["page"],
+    onclick: (info) => {
+      if (!info || !info["pageUrl"]) {
+        return;
+      }
+      const pageURL = info["pageUrl"];
+      toVRC(pageURL);
+    },
+  });
 
-    chrome.contextMenus.create({
-        title: 'SendVRC this page',
-        type: 'normal',
-        contexts: ['page'],
-        onclick: (info, tab) => {
-            if (!info || !info['pageUrl']) {
-                return;
-            }
-            const pageURL = info['pageUrl'];
-            toVRC(pageURL);
-        }
-    })
+  chrome.contextMenus.create({
+    title: "SendVRC with clipboard",
+    type: "normal",
+    contexts: ["browser_action"],
+    onclick: () => {
+      toVRC(getClipboard());
+    },
+  });
 
-    chrome.contextMenus.create({
-        title: 'SendVRC with clipboard',
-        type: 'normal',
-        contexts: ['browser_action'],
-        onclick: () => {
-            toVRC(getClipboard());
-        }
-    })
-
-    chrome.browserAction.onClicked.addListener((e) =>{
-        if (!e || !e['url']) {
-            return;
-        }
-        toVRC(e['url'])
-    });
-})()
+  chrome.browserAction.onClicked.addListener((e) => {
+    if (!e || !e["url"]) {
+      return;
+    }
+    toVRC(e["url"]);
+  });
+})();

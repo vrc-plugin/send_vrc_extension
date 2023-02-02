57f447799edb0926da7cab1a8efb3666a031c33f
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-02-02 22:52:57 +0000

    chore: setup webpack
    Signed-off-by: koyashiro <6698252+koyashiro@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: {
+    background: path.join(__dirname, "src/background.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({ patterns: [{ from: ".", to: ".", context: "static" }] }),
+  ],
+  devtool: "cheap-module-source-map",
+};
